6d65203af6c0a3007fcc65fa5a0fde8c18e39a2d
basharulalammazu <basharulalam6@gmail.com>
2025-04-19 21:49:00 +0600

    Load and explore Heart Disease dataset: view dimensions, column names, structure, summary stats, missing value count; convert 'BloodPressure' to numeric; encode 'Heart_Rate' categorical values; remove rows with missing 'Age'.

--- QuantumHealthPulse_Preprocessing.R
+++ QuantumHealthPulse_Preprocessing.R
@@ -1,3 +1,57 @@
-# DataSet Import
+# Load library
+library(dplyr)
+
+
+# Dataset Import
 dataset <- read.csv("D:/University/Semester 8/Data Science/MID/Project/HeartHealth-DataPreprocessing-R/Dataset/HeartDisease_QuantumData.csv", header = TRUE, sep = ",")
-print(dataset)
+
+
+# Number of rows and column dataset
+dim(dataset)
+
+
+# Columns name
+names(dataset)
+
+
+# View the first few rows
+head(dataset)
+
+
+# View the structure of the dataset
+str(dataset)
+
+
+# Summary of dataset
+summary(dataset)
+
+
+# Total Missing Value
+sum(is.na(dataset))
+
+
+
+dataset$BloodPressure <- as.numeric(dataset$BloodPressure)
+
+dataset$Heart_Rate[dataset$Heart_Rate == "Low"] <- 0
+dataset$Heart_Rate[dataset$Heart_Rate == "High"] <- 1
+dataset$Heart_Rate[dataset$Heart_Rate == ""] <- NA
+
+
+
+# Missing values per column
+colSums(is.na(dataset))
+
+
+# Discard Instances
+dataset <- dataset %>% filter(!is.na(Age))
+
+sum(is.na(dataset$Age))
+
+
+head(dataset)
+
+
+
+
+
